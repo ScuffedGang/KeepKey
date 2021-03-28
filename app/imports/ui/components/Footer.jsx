0bd86374bcddc720b86010b7edae044ccf6f058b
nzenger <nzenger@hawaii.edu>
2021-03-27 21:02:44 -1000

    First version of the landing page, not all information is accurate, just place holders.

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Icon } from 'semantic-ui-react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { color: 'white', backgroundColor: '#024731', padding: '15px 0' };
+    const divStyle = { color: 'blue', backgroundColor: '#FFA600', padding: '15px 0' };
     return (
           <div style={divStyle} className="ui fluid center aligned container" id='footer'>
             {/* eslint-disable-next-line react/jsx-no-target-blank */}
@@ -13,7 +13,7 @@ class Footer extends React.Component {
             <a href='https://twitter.com/UHM_StudentLife' target='_blank'><Icon name='twitter square'/></a>
             {/* eslint-disable-next-line react/jsx-no-target-blank */}
             <a href='https://github.com/cl-uh-b' target='_blank'><Icon name='github'/> <br /></a>
-            Website Managed by CL-UH-B <br />
+            Website Managed by KeepKey <br />
             Information Provided by Student Life & Development <br />
             Phone: (808) 956-8178 · Email: sld@hawaii.edu<br />
             University of Hawaiʻi at Mānoa · 2465 Campus Road, Room 208 · Honolulu, HI 96822
