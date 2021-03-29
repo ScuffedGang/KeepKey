c2b3e2c258fb357c1ede63a4df097b20645b5c75
Kevin Nguyen <nk279@hawaii.edu>
2021-03-28 17:14:47 -1000

    removed unnecessary code and renamed files

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,8 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import { Counts } from 'meteor/tmeasday:publish-counts';
 import { Keys } from '../../api/keys/Keys';
-import { Interests } from '../../api/interests/Interests';
-import { Favorites } from '../../api/favorites/Favorites';
 
 /** This subscription publishes only the Keys owned by the logged in user */
 Meteor.publish('MyKeys', function publish() {
@@ -18,44 +15,4 @@ Meteor.publish('Keys', function publish() {
     return Keys.find({});
   }
   return this.ready();
-});
-
-/** This subscription publishes all club interest */
-Meteor.publish('Interests', function publish() {
-  return Interests.find();
-});
-
-/** This subscription publishes only Keys favorited by user */
-Meteor.publish('Favorites', function publish() {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Favorites.find({ owner: username });
-  }
-  return this.ready();
-});
-
-/** This subscription publishes only the Keys that have been submitted to view. */
-Meteor.publish('Submitted', function publish() {
-  if (this.userId) {
-    return Keys.find({});
-  }
-  return this.ready();
-});
-
-Meteor.publish('LuckyKeys', function publish() {
-  if (this.userId) {
-    const interests = Meteor.users.findOne(this.userId).profile.interests;
-    if (interests.length !== 0) {
-      Counts.publish(this, 'LuckyCount', Keys.find({ }));
-      return Keys.find({ interest: { $in: interests } });
-    }
-      Counts.publish(this, 'LuckyCount', Keys.find({}));
-      return Keys.find({});
-
-  }
-  return this.ready();
-});
-
-Meteor.publish('ClubCount', function publish() {
-  Counts.publish(this, 'ClubCount', Keys.find({}));
 });
